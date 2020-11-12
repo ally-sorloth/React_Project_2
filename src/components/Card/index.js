5fda8383619d2e732e065a58b3aee365aef0fb48
allysorloth <allysorloth@gmail.com>
2020-11-12 22:17:59 +0300

    inline style

---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const cardStyle = {
+    color: 'red',
+    fontSize: 30,
+    backgroundColor: 'blue'
+}
+export const Card = () => {
+    return (
+        <div style={cardStyle}>
+            CARD
+        </div>
+    )
+}
